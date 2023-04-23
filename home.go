0e3ae7bbd0ff5e2472e75a4912447ac69f3759ff
Carlo <gitlab@iscienceworld.com>
2023-04-23 14:29:41 -0700

    home go and gohtml done

--- home.go
+++ home.go
@@ -8,12 +8,32 @@ import (
 type StudentPageHandlers interface {
 	GETStudentHandler(writer http.ResponseWriter, request *http.Request)
 }
-
-type HomeInfo struct {
+type PrizeData struct {
+	Name      string
+	Threshold int
+}
+type HomeData struct {
+	Name   string
+	Grade  int
+	Points int
+	//the following can be changed later, it was just convenient and I liked it as a solution, but it may not work
+	Grade9Points         []int
+	Grade10Points        []int
+	Grade11Points        []int
+	Grade12Points        []int
+	NinthWinners         []string
+	TenthWinners         []string
+	EleventhWinners      []string
+	TwelfthWinners       []string
+	RandomNinthWinner    string
+	RandomTenthWinner    string
+	RandomEleventhWinner string
+	RandomTwelfthWinner  string
+	Prizes               []PrizeData
 }
 
-func (e *HomeInfo) GETStudentHandler(writer http.ResponseWriter, request *http.Request) {
-	err := tplExec(writer, "home.gohtml", HomeInfo{})
+func (e *HomeData) GETStudentHandler(writer http.ResponseWriter, request *http.Request) {
+	err := tplExec(writer, "home.gohtml", *e)
 	//@TODO: REMOVE
 	if err != nil {
 		return
@@ -21,6 +41,7 @@ func (e *HomeInfo) GETStudentHandler(writer http.ResponseWriter, request *http.R
 }
 
 func tplExec2(w http.ResponseWriter, filename string, information any, filename2 string) error {
+	//tplExec may be different now because /webpages ???
 	temp := template.Must(template.ParseFiles(filename, filename2))
 
 	err := temp.Execute(w, information)
